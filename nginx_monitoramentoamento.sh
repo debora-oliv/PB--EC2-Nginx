681ac11832590b9c7dd870bf10b84a44527af624
Débora <deboraalves007@hotmail.com>
2025-07-01 08:54:47 -0300

    UpdateDir nginx_monitoramentoamento.sh
    Alteração no diretório do log e melhora no sistema de webhook - envia mensagens apenas se o servidor estiver offline.

--- nginx_monitoramentoamento.sh
+++ nginx_monitoramentoamento.sh
@@ -2,23 +2,15 @@
 
 NGINX_URL="http://localhost"
 DISCORD_WEBHOOK="URL_DO_SEU_WEBHOOK"
-LOG_DIR="/var/log/nginx-monitoramento"
-LOG_FILE="$LOG_DIR/status.log"
+LOG_DIR="/var/log/"
+LOG_FILE="$LOG_DIR/nginx-monitoramento.log"
 
 if curl -s -I "$NGINX_URL" | grep -q "200 OK"; then
-	STATUS="Servidor Nginx ONLINE - $(date '+%d/%m/%Y %H:%M:%S')"
-	echo "$STATUS" >> "$LOG_FILE"
+	echo "Servidor Nginx ONLINE - $(date '+%d/%m/%Y %H:%M:%S')" >> "$LOG_FILE"
 else
 	STATUS="Servidor Nginx OFFLINE - $(date '+%d/%m/%Y %H:%M:%S')"
 	echo "$STATUS" >> "$LOG_FILE"
-
-	curl -H "Content-Type: application/json" -X POST -d '{
-		"content": "'"$STATUS"'",
-		"flags": 4
-	}' "$DISCORD_WEBHOOK"
-
 	sudo systemctl restart nginx
-
 fi
 
 echo "$STATUS" >> "$LOG_FILE"
