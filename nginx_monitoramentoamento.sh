e618ac52d015379f3f30401ea6675e92f6069965
Débora <deboraalves007@hotmail.com>
2025-07-02 23:25:13 -0300

    Update nginx_monitoramentoamento.sh

--- nginx_monitoramentoamento.sh
+++ nginx_monitoramentoamento.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
 NGINX_URL="http://localhost"
-DISCORD_WEBHOOK="URL_DO_SEU_WEBHOOK"
+DISCORD_WEBHOOK="LINK_DO_SEU_WEEBHOOK"
 LOG_DIR="/var/log/"
 LOG_FILE="$LOG_DIR/nginx-monitoramento.log"
-ORIGEM="WSL"
+ORIGEM="EC2"
+
+mkdir -p "$LOG_DIR"
 
 if curl -s -I "$NGINX_URL" | grep -q "200 OK"; then
-        echo "[$ORIGEM] Servidor Nginx ONLINE - $(date '+%d/%m %H:%M')" >> "$LOG_FILE"
+	echo "[$ORIGEM] Servidor Nginx ONLINE - $(date '+%d/%m %H:%M')" >> "$LOG_FILE"
 else
-        STATUS="[$ORIGEM] Servidor Nginx OFFLINE - $(date '+%d/%m %H:%M')\nIP: $(curl -s ifconfig.me)"
-        echo "$STATUS" >> "$LOG_FILE"
+    	STATUS="[$ORIGEM] Servidor Nginx OFFLINE - $(date '+%d/%m %H:%M')"
+	echo "$STATUS" >> "$LOG_FILE"
 fi
 
 echo "$STATUS" >> "$LOG_FILE"
 
-curl -H "Content-Type: application/json" -X POST -d "{\"content\":\"$STATUS\"}" "$DISCORD_WEBHOOK"
+curl -sS -H "Content-Type: application/json" -X POST \
+    	-d "{\"content\":\"$STATUS\"}" "$DISCORD_WEBHOOK" \
+    	|| echo "[ERRO] Falha ao enviar para Discord - $(date '+%d/%m %H:%M')" >> "$LOG_FILE"
